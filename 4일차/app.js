62cd7341a8fb7bb3ae132b2c00fd8b2565fa8eb4
Nocha <Nocha@Nochaui-MacBook-Pro.local>
2016-12-23 19:00:42 +0900

    Nocha

--- 4일차/app.js
+++ 4일차/app.js
@@ -1,5 +1,18 @@
 let socket = io.connect('http://localhost:9999');
- 
-socket.on('connection', (socket) =>{
-    console.log("app");
+
+$('textarea#input_message').live('keypress', (e) => {
+	if (e.which == 13) {
+        e.preventDefault();
+        if($('textarea#input_message').val() != '')
+        {
+		    let textVal = $('textarea#input_message').val();
+            $('textarea#input_message').val('');
+
+            socket.emit('send message', {message: textVal});
+        }
+	}
+});
+
+socket.on('check alive', (data) => {
+    $('textarea#message').val(data.message).scrollTop(999999999);
 });
--- 4일차/server.js
+++ 4일차/server.js
@@ -1,5 +1,33 @@
 let io = require('socket.io')(9999);
+let chatingLogs = {};
+let nickNames = {};
 
-io.on('connection', (socket) => {
-    console.log(`${socket.id}`);
+io.on('connection', (client) => {
+    //console.log(`${client.id}`);   
+    let userName = 'user' + '[' + client.id.slice(0, 3) + ']';
+    nickNames[client.id] = userName;
+    chatingLogs[client.id] = '';
+    client.on('disconnect', () => {
+       // console.log(`해제 : ${client.id}`);
+    });
+
+    client.on('send message', (data) => {
+        if(data.message.split(' ')[0] == '/nickName')
+        {
+            userName = data.message.split(' ')[1];
+            nickNames[client.id] = userName;
+            console.log(nickNames[client.id]);
+        }
+        else
+        {
+            chatingLogs.forEach(function(element) {
+                element += userName + '\n' + data.message + '\n\n';
+                console.log("ok");
+            }, this);
+        }
+    });
+
+    setInterval(() => {
+        io.emit('check alive', {message: chatingLogs[client.id]})
+    }, 100);
 });
